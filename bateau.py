d1eb78ac2ffe011e89c52e0ac49b6e04d24cce5b
Ayman.Bouiba31 <carson.pawlyshine@gmail.com>
2024-03-19 09:53:09 +0100

    test

--- bateau.py
+++ bateau.py
@@ -9,6 +9,19 @@ class Bateau:
     cases_hit = int
 
     def __init__(self, nom, taille, orientation, cases_hit):
+        self.nom = nom
         self.taille = taille
         self.orientation = orientation
         self.cases_hit = cases_hit
+
+    def est_coule(self):
+        if self.taille == self.cases_hit:
+            print("Bateau coulé, vous avez perdu ce bateau")
+
+    def tirer(self, x, y):
+        # sens orientation du tir (si le joueur choisit de tirer en horizontale)
+        if x ==
+
+    def est_touche(self):
+        self.cases_hit = self.cases_hit + 1
+        print("Bateau touché")
